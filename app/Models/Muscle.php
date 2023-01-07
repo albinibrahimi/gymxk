bd99082a0e8cd086a8902dd116b4e8aec14933d3
albinibrahimi <ai41281@ubt-uni.net>
2023-01-07 12:27:30 +0100

    Muscles Table

---
+++ app/Models/Muscle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Muscle extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'image'
+    ];
+
+}
